107dcbde5be261c317fb830ea63753446a61ed57
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2017-06-08 11:25:18 +0200

    variance code refactor

--- app/services/classifier/naive_bayes_classifier.rb
+++ app/services/classifier/naive_bayes_classifier.rb
@@ -12,31 +12,19 @@ module Classifier
     attr_reader :values, :classify_param, :data_table
 
     def first_range_first_param_variance
-      total = first_range.reduce(0) do |sum, el|
-        sum += (first_range_first_param_mean - el.send(first_given_param)) **2
-      end
-      total/(first_range_count - 1)
+      variance(first_range, first_range_first_param_mean, first_given_param)
     end
 
     def first_range_second_param_variance
-      total = first_range.reduce(0) do |sum, el|
-        sum += (first_range_second_param_mean - el.send(second_given_param)) **2
-      end
-      total/(first_range_count - 1)
+      variance(first_range, first_range_second_param_mean, second_given_param)
     end
 
     def second_range_first_param_variance
-      total = second_range.reduce(0) do |sum, el|
-        sum += (second_range_first_param_mean - el.send(first_given_param)) **2
-      end
-      total/(second_range_count - 1)
+      variance(second_range, second_range_first_param_mean, first_given_param)
     end
 
     def second_range_second_param_variance
-      total = second_range.reduce(0) do |sum, el|
-        sum += (second_range_second_param_mean - el.send(second_given_param)) **2
-      end
-      total/(second_range_count - 1)
+      variance(second_range, second_range_second_param_mean, second_given_param)
     end
 
     def first_range_first_param_mean
@@ -55,14 +43,11 @@ module Classifier
       @second_range_second_param_mean ||= second_range.average(second_given_param)
     end
 
-    def first_range_count
-      @first_range_count ||= first_range.count
+    def variance(range, mean, param)
+      total = range.reduce(0) { |sum, el| sum += (mean - el.send(param)) **2 }
+      total/(range.count - 1)
     end
-
-    def second_range_count
-      @second_range_count ||= second_range.count
-    end
-
+    
     def first_range
       @first_range ||= data_table.where("#{classify_param} = ?", 0 )
     end
