29b4d604200cc726eef66ce6d33f29a63e957429
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2017-06-08 11:00:11 +0200

    add mean tests to another input

--- spec/services/gender_classifier/naive_bayes_classifier_spec.rb
+++ spec/services/gender_classifier/naive_bayes_classifier_spec.rb
@@ -17,20 +17,48 @@ describe Classifier::NaiveBayesClassifier do
     let(:value_hash) { { height: 150, weight: 50} }
     subject { described_class.new(value_hash, 'gender', Person) }
 
-    it 'calculate properly first range first param mean' do
-      expect(subject.send(:first_range_first_param_mean)).to eq 180.75
+    describe '#first_range_first_param_mean' do
+      it 'returns proper value' do
+        expect(subject.send(:first_range_first_param_mean)).to eq 180.75
+      end
+
+      it 'returns proper value for different input' do
+        create(:man, height: 200, weight: 120)
+        expect(subject.send(:first_range_first_param_mean)).to eq 184.6
+      end
     end
 
-    it 'calculate properly first range second param mean' do
-      expect(subject.send(:first_range_second_param_mean)).to eq 81.25
+    describe '#first_range_second_param_mean' do
+      it 'returns proper value' do
+        expect(subject.send(:first_range_second_param_mean)).to eq 81.25
+      end
+
+      it 'returns proper value for different input' do
+        create(:man, height: 200, weight: 120)
+        expect(subject.send(:first_range_second_param_mean)).to eq 89
+      end
     end
 
-    it 'calculate properly second range first param mean' do
-      expect(subject.send(:second_range_first_param_mean)).to eq 165
+    describe '#second_range_first_param_mean' do
+      it 'returns proper value' do
+        expect(subject.send(:second_range_first_param_mean)).to eq 165
+      end
+
+      it 'returns proper value for different input' do
+        create(:woman, height: 168, weight: 58)
+        expect(subject.send(:second_range_first_param_mean)).to eq 165.6
+      end
     end
 
-    it 'calculate properly second range second param mean' do
-      expect(subject.send(:second_range_second_param_mean)).to eq 59.25
+    describe '#second_range_first_param_mean' do
+      it 'returns proper value' do
+        expect(subject.send(:second_range_second_param_mean)).to eq 59.25
+      end
+
+      it 'returns proper value for different input' do
+        create(:woman, height: 168, weight: 58)
+        expect(subject.send(:second_range_second_param_mean)).to eq 59
+      end
     end
   end
 
