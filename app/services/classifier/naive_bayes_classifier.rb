c6f8fbd64d2cf68c9699e56823d27318cf7239f8
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2017-06-09 12:20:40 +0200

    refactor

--- app/services/classifier/naive_bayes_classifier.rb
+++ app/services/classifier/naive_bayes_classifier.rb
@@ -1,6 +1,5 @@
 module Classifier
   class NaiveBayesClassifier
-
     def initialize(values, classify_param, data_table)
       @values = values
       @classify_param = classify_param
@@ -8,29 +7,39 @@ module Classifier
     end
 
     def run
-      classify_param_hash = data_table.send(classify_param.pluralize)
-      result = classify_param_hash.reduce({}) do |hash, el|
-        range = data_table.where("#{classify_param} = ?", el[1])
-        hash[el[0]] = numerator_of_posterior(range)
-        hash
-      end
-      result.max_by{|k,v| v}[0]
+      calculate_posterior.max_by{|k,v| v}[0]
     end
-
+    
     private
 
     attr_reader :values, :classify_param, :data_table
 
-    def numerator_of_posterior(range)
-      likehood_total = values.each.reduce(1) do |product, el|
-        mean = mean(range, el[0])
-        product *= likelihood(el[1], mean, variance(range, el[0]))
+    def calculate_posterior
+      classify_param_hash.reduce({}) do |hash, el|
+        hash.update(el[0] => numerator_of_posterior(range(el[1])))
       end
-      range.count.to_f / data_table.count * likehood_total
+    end
+
+    def numerator_of_posterior(range)
+      range.count.to_f / data_table.count * likehood_total(range)
+    end
+
+    def likehood_total(range)
+      values.each.reduce(1) do |product, el|
+        product *= likelihood(el[1], mean(range, el[0]), variance(range, el[0]))
+      end
     end
 
     def likelihood(value, mean, variance)
       1/(Math.sqrt(2*Math::PI*variance)) * Math.exp(( - (value - mean)**2)/(2*variance))
+    end
+
+    def classify_param_hash
+      data_table.send(classify_param.pluralize)
+    end
+
+    def range(value)
+      data_table.where("#{classify_param} = ?", value)
     end
 
     def variance(range, param)
@@ -40,22 +49,6 @@ module Classifier
 
     def mean(range, param)
       range.average(param)
-    end
-
-    def first_range
-      @first_range ||= data_table.where("#{classify_param} = ?", 0 )
-    end
-
-    def second_range
-      @second_range ||= data_table.where("#{classify_param} = ?", 1 )
-    end
-
-    def first_given_param #weight
-      @first_given_param ||= values.keys.first
-    end
-
-    def second_given_param #height
-      @second_given_param ||= values.keys.last
     end
   end
 end
