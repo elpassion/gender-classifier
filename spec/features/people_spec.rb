fa3666a039ca189f7e3841e847ea0893cca1cc94
Katarzyna Laszczewska <katarzyna.laszczewska@gmail.com>
2017-06-12 11:33:38 +0200

    navbar buttons

--- spec/features/people_spec.rb
+++ spec/features/people_spec.rb
@@ -5,14 +5,29 @@ describe 'when visiting people page' do
   let!(:male_2) { create(:man, height: 190, weight: 90)}
   let!(:female_1) { create(:woman, height: 160, weight: 50)}
 
-  before { visit people_path }
+  context 'within root path' do
+    before { visit root_path }
 
-  it 'should see List of People headline' do
-    expect(page).to have_content 'List of People'
+    it 'should contain test data link' do
+      expect(page).to have_link 'Test data', href: people_path
+    end
+
+    it 'should move to people path' do
+      click_link 'Test data'
+      expect(page).to have_current_path people_path
+    end
   end
 
-  it 'should contain list of people' do
-    expect(page).to have_selector('table tr', count: 4)
-    expect(find_all('tr').last.text).to match female_1.gender
+  context 'within people path' do
+    before { visit people_path }
+
+    it 'should see List of People headline' do
+      expect(page).to have_content 'List of People'
+    end
+
+    it 'should contain list of people' do
+      expect(page).to have_selector('table tr', count: 4)
+      expect(find_all('tr').last.text).to match female_1.gender
+    end
   end
 end
